ab6e70697b6ed0b6ae10aa86e87cffc634a68b72
olga.shishkina <shishkina-oyu@ural.rt.ru>
2022-10-20 12:14:48 +0500

    ДЗ-2

--- src/main/kotlin/homework2/task1/Task1.kt
+++ src/main/kotlin/homework2/task1/Task1.kt
@@ -6,7 +6,21 @@ fun main() {
     println("На вход подается одно натуральное трехзначное число - N. Напишите код, который будет вычислять сумму цифр в данном числе.")
     print("Введите N: ")
     val input = Scanner(System.`in`)
-    val inputText = input.next()
-    val sum = inputText[0].digitToInt() + inputText[1].digitToInt() + inputText[2].digitToInt()
-    println("Cумма цифр в данном числе: $sum")
+    var inputText = input.next().trim()
+    try {
+        if(inputText.toLong() < 0 || !(inputText.toInt() in 100..999)){
+            println("Попробуйте ещё раз. Введите натуральное трехзначное число")
+            inputText = input.next().trim()
+            if (inputText.toLong() < 0 || !(inputText.toInt() in 100..999)) {
+                println("Всё пропало!")
+                return
+            }
+        }
+        val inputInt = inputText.toInt().toString()
+        val sum = inputInt[0].digitToInt() + inputInt[1].digitToInt() + inputInt[2].digitToInt()
+        println("Cумма цифр в данном числе: $sum")
+    } catch (e: NumberFormatException){
+        println("Всё пропало! Это не натуральное трехзначное число")
+    }
+
 }
--- src/main/kotlin/homework2/task2/Task2.kt
+++ src/main/kotlin/homework2/task2/Task2.kt
@@ -6,11 +6,20 @@ fun main(){
     println("На вход подается одно натуральное число N - количество минут. Напишите код, который будет конвертировать минуты в кол-во годов и месяцев.")
     print("Введите N: ")
     val input = Scanner(System.`in`)
-    val inputLong = input.nextLong()
-    val hours = inputLong/60
-    val days = hours/24
-    var months = days/30
-    val years = days/360
-    months -= years * 360/30
-    println("$years лет, $months месяцев")
+    val inputText = input.next()
+    try {
+        val inputLong = inputText.toLong()
+        if(inputLong < 0){
+            println("Введенные данные не удовлетворяют условию")
+            return
+        }
+        val hours = inputLong/60
+        val days = hours/24
+        var months = days/30
+        val years = days/360
+        months -= years * 360/30
+        println("$years лет, $months месяцев")
+    } catch (e: NumberFormatException){
+        println("Введенные данные не удовлетворяют условию")
+    }
 }
